693dcd41bc0d9c7624bea5e64f5913b02f8fec4e
Shameer <shameerahamed.f@gmail.com>
2022-05-29 01:33:36 +0800

    Update config.js

--- services/firebase/config.js
+++ services/firebase/config.js
@@ -1,9 +1,9 @@
 export const config = {
-  apiKey: 'AIzaSyAjJG6UrfA1FEzV8qbjmuk8wiUic0uWv-8',
-  authDomain: 'finapp-17474.firebaseapp.com',
-  databaseURL: 'https://finapp-17474.firebaseio.com',
-  projectId: 'finapp-17474',
-  storageBucket: 'finapp-17474.appspot.com',
-  messagingSenderId: '1057184379213',
-  appId: '1:1057184379213:web:8c728d0b7099f194',
-}
+  apiKey: "AIzaSyDs6X5qrwg4ccu5QoPNHPMGlrNWlBNTwOg",
+  authDomain: "finapp-94ff0.firebaseapp.com",
+  databaseURL: "https://finapp-94ff0-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "finapp-94ff0",
+  storageBucket: "finapp-94ff0.appspot.com",
+  messagingSenderId: "66087808430",
+  appId: "1:66087808430:web:4f31c79f838de5cc01ec3f"
+};
